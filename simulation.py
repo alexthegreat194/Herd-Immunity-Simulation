c5bb541ecf7032d9260164c450afd26e90071c26
alexthegreat194 <alexander.gentry@aol.com>
2021-12-06 17:50:26 -0800

    FINALLY FIXED IT

--- simulation.py
+++ simulation.py
@@ -2,6 +2,7 @@ import random
 import sys
 from person import Person, State
 from virus import Virus
+from time import sleep, time
 
 class Simulation:
     def __init__(self, pop_size, vac_perc, virus_name, rep_rate, mort_rate, init_infected):
@@ -31,14 +32,14 @@ class Simulation:
         #returns a list containing the indices of infect people objects
         infected_indices = []
         for i in range(len(self.people)):
-            if self.people[i].infected:
+            if self.people[i].infected == True and self.people[i].dead == False:
                 infected_indices.append(i)
         return infected_indices
 
     def normal_people(self):
         normal_indices = []
         for i in range(len(self.people)):
-            if not self.people[i].infected and not self.people[i].vaccinated:
+            if self.people[i].infected == False and self.people[i].vaccinated == False and self.people[i].dead == False:
                 normal_indices.append(i)
         return normal_indices
 
@@ -50,34 +51,40 @@ class Simulation:
         return indices
 
     def step(self):
-        '''
-        MAKE A LIST OF PAST INFECTED PEOPLE AS A GLOBAL LIST !!!! AND AS KAROUND
-        '''
         # find all infected people
         infected_indices = self.infected_people()
+        new_infections = []
+        
+        amt = 0
+        for person in self.people:
+            if person.infected == False and person.dead == False and person.vaccinated == False:
+                amt += 1
+        print(f"amt: {amt}")
 
         # calculate interactions
+        normal_people = self.normal_people()
         for i in infected_indices:
-            infected_person = self.people[i]
             for x in range(100):
-                buffer = random.choice(self.people)
-                probability = float(random.randrange(0, 100))/100
-                if probability < self.virus.rep_rate:
-                    if not buffer.vaccinated and not buffer.dead:
-                        buffer.infected = True
-
-        # RECALCULATE infected people
-        infected_indices = self.infected_people()
+                normal_index = random.choice(normal_people)
+                if self.virus.rep_rate >= random.random():
+                    new_infections.append(normal_index)
 
         # calculate dead people
         for i in infected_indices:
-            buffer = self.people[i]
-            probability = float(random.randrange(0, 100))/100
-            if probability < self.virus.mort_rate:
-                buffer.dead = True
+            probability = random.random()
+            if probability <= self.virus.mort_rate:
+                self.people[i].vaccinated = True
             else:
-                buffer.vaccinated = True
+                self.people[i].dead = True
+            self.people[i].infected = False
 
+        #infect new people
+        print(f"infected {len(new_infections)} people")
+        for i in new_infections:
+            self.people[i].infected = True
+            # print(i)
+            # print(self.people[i].infected)
+        print(f"found infected: {len(self.infected_people())}")
 
 
 if __name__ == '__main__':
@@ -87,13 +94,14 @@ if __name__ == '__main__':
     arg_list = sys.argv
     simulation = Simulation( int(arg_list[1]), float(arg_list[2]), str(arg_list[3]), float(arg_list[4]), float(arg_list[5]), int(arg_list[6]) )
     
+    random.seed(time())
     print("Simulations STARTING")
     steps = 0
     while len(simulation.normal_people()) > 0:
+        print("")
         simulation.step()
         steps += 1
-
-        print("")
+        
         print(f"Infected: {len(simulation.infected_people())}")
         print(f"Dead: {len(simulation.dead_people())}")
         print(f"Normal: {len(simulation.normal_people())}")
